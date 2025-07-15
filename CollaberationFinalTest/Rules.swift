70cad61be94f173ce0665a72b8dd574f410f0a77
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-15 13:28:05 -0500

    Nothing in file

---
+++ CollaberationFinalTest/Rules.swift
@@ -0,0 +1,14 @@
+//
+//  Rules.swift
+//  CollaberationFinalTest
+//
+//  Created by Tirzaan on 7/15/25.
+//
+// MARK: WYATT
+// last check up 7/15/25/1:03pm
+//Notes ⚠️
+/*
+ there is nothing here
+ */
+
+
